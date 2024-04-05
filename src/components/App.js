b1c000ffc38548c6b73b7b0d078e3ddccc994091
Swati jagtap <swatijagtap5060@gmail.com>
2024-04-05 18:31:22 +0530

    submit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [isclick,setClick]=useState(false)
+  function handlclick(){
+    setClick(!isclick)
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+        <button onClick={handlclick}>Show Modal</button>
+        <div className="model-overlay">
+          {isclick && <div>
+            <button className="model-close" onClick={handlclick}>close</button>
+           <p className="model-p">This is a content of modal.</p>
+          </div>}
+           
+        </div>
     </div>
   )
 }
